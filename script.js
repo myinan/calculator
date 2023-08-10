e3073a46e424236ec63bb7664a5103178a905b1e
myinan <m.yasir.inan@gmail.com>
2023-08-10 17:40:30 +0300

    Display button values on screen

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Define function for calculator operations
 function operate(firstNum, secondNum, operator) {
     switch (operator) {
         case "+":
@@ -11,3 +12,14 @@ function operate(firstNum, secondNum, operator) {
    };
 };
 
+
+// Get button values to screen (with event delegation), store the number in currentValue
+const screen = document.querySelector(".operation-screen");
+let currentValue;
+const calculator = document.querySelector(".calculator-container");
+calculator.addEventListener("click", item => {
+    if(item.target.value) {
+        screen.textContent += item.target.value;
+        currentValue = +screen.textContent;
+    };
+});
