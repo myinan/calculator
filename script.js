83722b4b38f1cf4bf09bc63f3edc20cbc9a7861b
myinan <m.yasir.inan@gmail.com>
2023-08-16 14:50:03 +0300

    Refactor function definitions

--- script.js
+++ script.js
@@ -16,16 +16,56 @@ function operate(firstNum, operator, secondNum) {
    };
 };
 
-
 // Declare variables
 const operators = ["+", "-", "*", "/"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const events = ["click", "keydown"];
 
-
 // Access HTML elements
 const screenTop = document.querySelector(".screen-top");
 const screenBottom = document.querySelector(".screen-bottom");
+
+
+// Define handler functions
+function handleNumberClick(number) {
+    if (screenBottom.textContent == "" && number == 0) return;
+    screenBottom.textContent += number;
+}
+
+function handleOperatorClick(operator) {
+    if (screenBottom.textContent != "") {
+        if (operators.includes(screenBottom.textContent[screenBottom.textContent.length - 1])) return;
+        arr = Array.from(screenBottom.textContent);
+        filteredArr = arr.filter( item => operators.includes(item));
+        if (filteredArr.length == 0) screenBottom.textContent +=operator;
+        else if (filteredArr.length == 1 && screenBottom.textContent[0] == "-") {
+            screenBottom.textContent +=operator;
+        } else if (filteredArr.length > 1) return;
+    }
+    else if (screenBottom.textContent == "" && operator == "-") screenBottom.textContent +=operator;
+    else return;
+}
+
+//Populate arguments object and call the operate() function
+function handleEqualsClick() {
+    if (screenBottom.textContent != "") {
+        let bottomArray = Array.from(screenBottom.textContent);
+        bottomArray.forEach((item, index) => {
+            if (index != 0 && operators.includes(item)) {
+                let firstNum = screenBottom.textContent.slice(0, index);
+                let operator = item;
+                let secondNum = screenBottom.textContent.slice(index + 1);
+                if (secondNum == "") return;
+                let solution = operate(firstNum, operator, secondNum);
+                screenTop.textContent = screenBottom.textContent;
+                screenBottom.textContent = +solution.toFixed(3);
+                return;
+            }
+            else return;
+        });
+    }
+    else return;
+}
 
 
 // Event delegation and handlers
@@ -35,46 +75,6 @@ events.forEach(listenEvent => {
         if (event.target.value) targetValue = event.target.value;
         else if (event.key) targetValue = event.key;
 
-        function handleNumberClick(number) {
-            if (screenBottom.textContent == "" && number == 0) return;
-            screenBottom.textContent += number;
-        }
-    
-        function handleOperatorClick(operator) {
-            if (screenBottom.textContent != "") {
-                if (operators.includes(screenBottom.textContent[screenBottom.textContent.length - 1])) return;
-                arr = Array.from(screenBottom.textContent);
-                filteredArr = arr.filter( item => operators.includes(item));
-                if (filteredArr.length == 0) screenBottom.textContent +=operator;
-                else if (filteredArr.length == 1 && screenBottom.textContent[0] == "-") {
-                    screenBottom.textContent +=operator;
-                } else if (filteredArr.length > 1) return;
-            }
-            else if (screenBottom.textContent == "" && operator == "-") screenBottom.textContent +=operator;
-            else return;
-        }
-    
-        //Populate arguments object and call the operate() function
-        function handleEqualsClick() {
-            if (screenBottom.textContent != "") {
-                let bottomArray = Array.from(screenBottom.textContent);
-                bottomArray.forEach((item, index) => {
-                    if (index != 0 && operators.includes(item)) {
-                        let firstNum = screenBottom.textContent.slice(0, index);
-                        let operator = item;
-                        let secondNum = screenBottom.textContent.slice(index + 1);
-                        if (secondNum == "") return;
-                        let solution = operate(firstNum, operator, secondNum);
-                        screenTop.textContent = screenBottom.textContent;
-                        screenBottom.textContent = +solution.toFixed(3);
-                        return;
-                    }
-                    else return;
-                });
-            }
-            else return;
-        }
-    
         // Handle different button clicks based on their value
         if (numbers.includes(targetValue)) {
             handleNumberClick(targetValue);
@@ -85,8 +85,6 @@ events.forEach(listenEvent => {
         }
     });
 });
-
-
 
 // Event handler for "clear" and "delete" buttons
 events.forEach(listenEvent => {
